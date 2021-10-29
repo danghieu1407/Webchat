50bd7521adcefa15391885d022fde46fc0236af2
danghieu140701@gmail.com <danghieu140701@gmail.com>
2021-10-29 10:30:28 +0700

    zo

---
+++ TieuLuan/Client.js
@@ -0,0 +1,44 @@
+
+(function () {
+    const sendBtn = document.querySelector('#send');
+    const messages = document.querySelector('#messages');
+    const messageBox = document.querySelector('#messageBox');
+
+    let ws;
+
+    function showMessage(message) {
+        messages.textContent += `\n\n${message}`;
+        messages.scrollTop = messages.scrollHeight;
+        messageBox.value = '';
+    }
+
+    function init() {
+        if (ws) {
+            ws.onerror = ws.onopen = ws.onclose = null;
+            ws.close();
+        }
+
+        ws = new WebSocket('ws://localhost:6969');
+        ws.onopen = () => {
+            console.log('Connection opened!');
+        }
+        ws.onmessage = ({ data }) => showMessage(data);
+        ws.onclose = function () {
+            ws = null;
+        }
+    }
+
+    sendBtn.onclick = () => {
+        if (!ws) {
+            showMessage('No WebSocket connection :<');
+            return;
+        }
+        else
+        {
+            ws.send(messageBox.value);
+            showMessage(messageBox.value);
+        }
+    }
+
+    init();
+})();
